318b52e97ec040af598b3325f1c34d6fa97d5478
Сергей Сафонов <s-safik@mail.ru>
2020-03-19 16:44:47 +0300

    Глава 14. Базы данных SQLite. - Добавлены классы работы с БД; события в списке теперь хранятся не в локальных объектах, а в БД приложения.

--- app/src/main/java/ru/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -115,6 +115,7 @@ public class CrimeListFragment extends Fragment {
             mCrimeRecyclerView.setAdapter(mAdapter);
         }
         else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -178,6 +179,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
     }
 }
